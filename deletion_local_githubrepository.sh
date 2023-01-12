3ac9501d039c6225dc3f1fb114b66484744b71dd
Xue Zhang <zhangxue911@hotmail.com>
2023-01-12 11:31:56 +0100

    Add files via upload

---
+++ deletion_local_githubrepository.sh
@@ -0,0 +1,11 @@
+# go to the current working repository 
+cd RNAPII_collision_at_Rosa_group
+# lsit all the files in this repository
+ls -a 
+# delete the folders and files
+rm -rf RNAPII_collision_at_Rosa_group
+RM -rf .git
+# confirm if everything is deleted
+ls
+# can start to reclone the repository from github website
+git clone https://github.com/xuezhang911/RNAPII_collision_at_Rosa_group.git
